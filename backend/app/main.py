8d61f1dca4231c443d5ac7b2b6a9ca9473216522
Narendranadh G <narendranadh599@gmail.com>
2026-06-29 19:22:51 +0530

    Add initial API Endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,36 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def home():
     return {
-    "company": "Genesis AI",
-    "product": "Atlas",
-    "status": "Development Started",
-    "founder": "Narendranadh G",
-    "version": "0.0.1"
+        "company": "Genesis AI",
+        "product": "Atlas"
+    }
+
+@app.get("/about")
+def about():
+    return {
+        "founder": "Narendranadh G",
+        "vision": "Build Enterprise AI Solutions"
+    }
+
+@app.get("/version")
+def version():
+    return {
+        "version": "0.0.1",
+        "environment": "development"
+    }
+
+@app.get("/Profile")
+def profile():
+    return {
+        "name": "Narendranadh G",
+        "role": "Founder",
+        "company": "Genesis AI",
+        "skills": [
+            "SAP BW",
+            "Python",
+            "FastAPI"
+        ]
     }
